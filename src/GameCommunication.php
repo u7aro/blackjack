9a30e52b20189e4a7c489a834f469649c1352e67
Yutaro Ohno <yutaro.ohno@studio-umi.jp>
2018-06-28 17:18:35 +0900

    インターフェイスのコメントを拡充

--- src/GameCommunication.php
+++ src/GameCommunication.php
@@ -3,6 +3,16 @@
 namespace Blackjack;
 
 interface GameCommunication {
+  /**
+   * 「ヒット」を宣言して追加のカードを貰うか判定する.
+   *
+   * @return bool
+   *   追加のカードを望む場合は TRUE、望まない場合は FALSE.
+   */
   public function hits();
+
+  /**
+   * カードを受け取る.
+   */
   public function addCard();
 }
