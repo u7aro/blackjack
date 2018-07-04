291721f98e33d12d26e475d0e5ff1fc586aebf59
Yutaro Ohno <yutaro.ohno@studio-umi.jp>
2018-07-05 00:50:26 +0900

    コメントの追加

--- src/GameCommunication.php
+++ src/GameCommunication.php
@@ -2,7 +2,11 @@
 
 namespace Blackjack;
 
+/**
+ * Game クラスのゲームに必要な情報をやり取りするためのインターフェイス.
+ */
 interface GameCommunication {
+
   /**
    * 「ヒット」を宣言して追加のカードが必要か真偽値で返す.
    *
@@ -14,13 +18,17 @@ interface GameCommunication {
   /**
    * カードを受け取る.
    *
-   * @param sting $card
-   *   `A-11` のフォーマットで生成されたテキストのカード文字列.
+   * @param object $card
+   *   Card クラスのインスタンスオブジェクト.
    */
   public function addCard($card);
 
   /**
    * プレイヤーの名前を返す.
+   *
+   * @return string
+   *   プレイヤー名の文字列.
    */
   public function getName();
+
 }
