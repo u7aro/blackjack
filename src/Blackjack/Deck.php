c5e498e512947899ded79d62b8c224c08e406a6f
Yutaro Ohno <yutaro.ohno@studio-umi.jp>
2018-07-13 16:06:54 +0900

    デッキの生成ロジックをメソッド化

--- src/Blackjack/Deck.php
+++ src/Blackjack/Deck.php
@@ -15,23 +15,31 @@ class Deck {
   private $cards = [];
 
   /**
-   * 指定されたカードデッキの数だけカードを生成.
+   * Deck オブジェクトを構築する.
    *
    * @param int $num_decks
    *   生成するデッキの数.
    */
   public function __construct($num_decks = 1) {
+    $this->cards = Deck::generateCards($num_decks);
+    shuffle($this->cards);
+  }
+
+  /**
+   * 指定された数だけカードデッキを生成する.
+   */
+  public static function generateCards($num_decks = 1) {
+    $cards = [];
     for ($num_decks_i = 0; $num_decks_i < $num_decks; $num_decks_i++) {
       // 各アルファベットの意味:
       //   S: スペード, D: ダイヤ, H: ハート, C: クラブ
       foreach (['S', 'D', 'H', 'C'] as $suit) {
         for ($number_i = 1; $number_i <= 13; $number_i++) {
-          $this->cards[] = new Card($suit, $number_i);
+          $cards[] = new Card($suit, $number_i);
         }
       }
     }
-
-    shuffle($this->cards);
+    return $cards;
   }
 
   /**
