480a635dae35460d4f1231d92e31f903b01dafd1
Yutaro Ohno <yutaro.ohno@studio-umi.jp>
2018-07-13 11:28:05 +0900

    Card クラスのテストを追加

---
+++ tests/CardTest.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * カードクラスのテストを行うクラス.
+ */
+class CardTest extends PHPUnit\Framework\TestCase {
+
+  static function forTestGetPoint() {
+    return [
+      '1 は 1 として数える' => [1, 1],
+      '10 は 10 として数える' => [10, 10],
+      '11 は 10 として数える' => [10, 11],
+      '12 は 10 として数える' => [10, 12],
+      '13 は 10 として数える' => [10, 13],
+    ];
+  }
+
+  /**
+   * getPoint() メソッドのテスト.
+   * @dataProvider forTestGetPoint
+   */
+  public function testGetPoint($excepted, $number) {
+    // 絵柄は何でも良いので D に固定.
+    $card = new Blackjack\Card('D', $number);
+    $this->assertEquals($excepted, $card->getPoint());
+  }
+
+}
