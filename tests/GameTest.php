3a9b7cef8ba7ae66dec302463602a77964ed8d5c
Yutaro Ohno <yutaro.ohno@studio-umi.jp>
2018-07-13 11:54:45 +0900

    calculateMinSum() メソッド用のテストを追加

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -34,4 +34,33 @@ class GameTest extends PHPUnit\Framework\TestCase {
     $this->assertEquals($excepted, Blackjack\Game::calculateSum($cards));
   }
 
+  static function forTestCalculateMinSum() {
+    return [
+      '2, 3 の組み合わせで 5 として計算されるか' => [
+      5,
+        [new Blackjack\Card('S', 2), new Blackjack\Card('H', 3)],
+      ],
+      '1, 10 の組み合わせで 11 として計算される' => [
+        11,
+        [new Blackjack\Card('S', 1), new Blackjack\Card('H', 10)],
+      ],
+      '1, 10, 10 の組み合わせで 21 として計算される' => [
+        21,
+        [new Blackjack\Card('S', 1), new Blackjack\Card('H', 10), new Blackjack\Card('D', 10)],
+      ],
+      '1, 1, 1, 1 の組み合わせで 4 として計算される' => [
+        4,
+        [new Blackjack\Card('S', 1), new Blackjack\Card('H', 1), new Blackjack\Card('D', 1), new Blackjack\Card('C', 1)],
+      ],
+    ];
+  }
+
+  /**
+   * ポイント計算が期待通りかテスト.
+   * @dataProvider forTestCalculateMinSum
+   */
+  public function testCalculateMinSum($excepted, $cards) {
+    $this->assertEquals($excepted, Blackjack\Game::calculateMinSum($cards));
+  }
+
 }
