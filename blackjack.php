081127fcb22e3136923968e50808e6faea00d088
Yutaro Ohno <yutaro.ohno@studio-umi.jp>
2018-07-19 01:20:34 +0900

    配列のフォーマットを見やすくなるように修正

--- blackjack.php
+++ blackjack.php
@@ -12,19 +12,23 @@ require 'vendor/autoload.php';
 // コンソールオプションの設定.
 $strict = in_array('--strict', $_SERVER['argv']);
 $arguments = new \cli\Arguments(compact('strict'));
-$arguments->addFlag(array('help', 'h'), 'Show this help screen');
-$arguments->addOption(array('name', 'n'), array(
-  'default'     => 'You',
-  'description' => 'あなたの名前を設定します'));
-$arguments->addOption(array('wait', 'w'), array(
-  'default'     => 500000,
-  'description' => '処理を実行する時の待ち時間(ms)を設定します'));
-$arguments->addOption(array('num-ai', 'a'), array(
-  'default'     => 0,
-  'description' => 'AIプレイヤーの数を設定します'));
-$arguments->addOption(array('pack', 'p'), array(
+$arguments->addFlag(['help', 'h'], 'Show this help screen');
+$arguments->addOption(['name', 'n'], [
+  'default' => 'You',
+  'description' => 'あなたの名前を設定します',
+]);
+$arguments->addOption(['wait', 'w'], [
+  'default' => 500000,
+  'description' => '処理を実行する時の待ち時間(ms)を設定します',
+]);
+$arguments->addOption(['num-ai', 'a'], [
+  'default' => 0,
+  'description' => 'AIプレイヤーの数を設定します',
+]);
+$arguments->addOption(['pack', 'p'], [
   'default'     => 1,
-  'description' => '使用するカードのパック数を設定します'));
+  'description' => '使用するカードのパック数を設定します',
+]);
 $arguments->parse();
 
 if ($arguments['help']) {
